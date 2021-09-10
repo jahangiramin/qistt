552a40f326d32860fb1ca69c6a44b7fd769fa8ab
Jahangir <jahangir.ey@gmail.com>
2021-09-10 20:04:14 +0500

    Database design for Product part

--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
+from os import name
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Term(models.Model):
+    name = models.CharField(max_length=255)
+
+class Paymenttype(models.Model):
+    name = models.CharField(max_length=255)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ManyToManyField(Category)
+    sku = models.CharField(max_length=255)
+    ispublic = models.BooleanField()
+
+class Priceplan(models.Model):
+    name = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, on_delete=CASCADE)
+    term = models.ForeignKey(Term, on_delete=CASCADE)    
+
+class Priceplandetail(models.Model):
+    priceplan = models.ForeignKey(Priceplan, on_delete=CASCADE)
+    description = models.CharField(max_length=255)
+    paymenttype = models.ForeignKey(Paymenttype, on_delete=CASCADE)
+    amount = models.IntegerField()
